a69890f4ffb34678dda0c55cddc12f71a8b63f3d
Scotti <matheusscotti23@hotmail.com>
2025-03-24 16:50:18 -0300

    first commit

---
+++ src/main/java/com/treinamento/alfa/entities/Order.java
@@ -0,0 +1,76 @@
+package com.treinamento.alfa.entities;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity
+@Table( name = "tb_order")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant momento;
+    private OrderStatus status;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private User client;
+
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
+
+    public Order () {
+
+    }
+
+    public Order(Long id, Instant momento, User client, OrderStatus status, Payment payment) {
+        this.id = id;
+        this.momento = momento;
+        this.client = client;
+        this.status = status;
+        this.payment = payment;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMomento() {
+        return momento;
+    }
+
+    public void setMomento(Instant momento) {
+        this.momento = momento;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public User getClient() {
+        return client;
+    }
+
+    public void setClient(User client) {
+        this.client = client;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
+}
---
+++ src/main/java/com/treinamento/alfa/entities/OrderStatus.java
@@ -0,0 +1,7 @@
+package com.treinamento.alfa.entities;
+
+public enum OrderStatus {
+
+    WAITING_PAYMENT, PAID, SHIPPED, DELIVERED, CANCELED;
+
+}
